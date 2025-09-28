4229c46b9be728d0cb387a6f6bcbdf51afd28f5b
Tawsif-jj <tawsif4060@gmail.com>
2025-09-28 22:18:29 +0600

    2nd commit

---
+++ fun.py
@@ -0,0 +1,5 @@
+def employ(employName):
+    print(employName , " ,Tore chakri dimu na.")
+
+employ1 = employ("Jubayer")
+
---
+++ main.py
@@ -0,0 +1,22 @@
+# def add(a,b):
+#     c = a * b
+#     print(c)
+
+# add(10,20)
+
+
+
+
+
+
+
+
+# def kidsName(*kids):
+#     print("The youngest child is " + kids[3])
+
+# kidsName("Emil", "Tobias", "Linus", "Tawsif")
+
+# def Desh(country = "Bangladesh"):
+
+#     Desh("I am from " + country)
+
